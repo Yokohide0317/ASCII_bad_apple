98f49cfccb9cdc3835f38c104a45d64626d10e71
Yokohide0317 <yokohide0317@gmail.com>
2022-04-28 13:10:42 +0900

    For python3

--- generate_ascii_art.py
+++ generate_ascii_art.py
@@ -1,10 +1,15 @@
 from PIL import Image
+import argparse
+import os
+import time
+import cv2
+from tqdm import tqdm
 
 video_length = 218
 
 ASCII_CHARS = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
 
-def scale_image(image, new_width=100, new_height=30):
+def scale_image(image, new_width, new_height):
     """Resizes an image preserving the aspect ratio.
     """
     (original_width, original_height) = image.size
@@ -31,7 +36,7 @@ def map_pixels_to_ascii_chars(image, range_width=3.69):
 
     return "".join(pixels_to_chars)
 
-def convert_image_to_ascii(image, new_width=100, new_height=30):
+def convert_image_to_ascii(image, new_width=150, new_height=45):
     image = scale_image(image, new_width, new_height)
     image = convert_to_grayscale(image)
 
@@ -39,7 +44,7 @@ def convert_image_to_ascii(image, new_width=100, new_height=30):
     len_pixels_to_chars = len(pixels_to_chars)
 
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+            range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
@@ -47,30 +52,39 @@ def handle_image_conversion(image_filepath):
     image = None
     try:
         image = Image.open(image_filepath)
-    except Exception, e:
-        print "Unable to open image file {image_filepath}.".format(image_filepath=image_filepath)
-        print e
+    except:
+        print("Unable to open image file {image_filepath}.".format(image_filepath=image_filepath))
         return
     image_ascii = convert_image_to_ascii(image)
     return image_ascii
 
 if __name__=='__main__':
-    import os
-    import time 
-    import cv2
-    vidcap = cv2.VideoCapture('video.mp4')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=True)
+    args = parser.parse_args()
+    input_path = args.input
+    txt_file = os.path.join(input_path, "play.txt")
+    mp4_file = os.path.join(input_path, "video.mp4")
+    jpg_file = os.path.join(input_path, "output.jpg")
+
+
+    vidcap = cv2.VideoCapture(mp4_file)
     time_count = 0
     frames = []
-    while time_count <= video_length*1000:
-        print('Generating ASCII frame at ' + str(time_count))
+    total = int(video_length*1000)
+    pbar = tqdm(total=total)
+    while time_count <= total:
+        #print('Generating ASCII frame at ' + str(time_count))
         vidcap.set(0, time_count)
         success, image = vidcap.read()
         if success:
-            cv2.imwrite('output.jpg', image)
-        frames.append(handle_image_conversion('output.jpg'))
+            cv2.imwrite(jpg_file, image)
+        frames.append(handle_image_conversion(jpg_file))
         time_count = time_count + 100
+        pbar.update(100)
+    pbar.close()
 
-    f = open('play.txt', 'w')
+    f = open(txt_file, 'w')
     f.write('SPLIT'.join(frames))
     f.close()
 
--- run.py
+++ run.py
@@ -1,16 +1,25 @@
 import time, os
+import argparse
+
 
 if __name__ == '__main__':
-	f = open('play.txt', 'r')
-	frame_raw = f.read()
-	frame_raw = frame_raw.replace('.', ' ')
-	f.close()
-	frames = frame_raw.split('SPLIT')
-	os.system('mplayer -vo null -vc null video.mp4 &')
-	#os.system('mplayer test.mp4 &')
-	init_time = time.time()
-	while time.time() <= init_time + 218:
-		os.system('clear')
-		print(frames[int((time.time()-init_time)*10)])
-		time.sleep(0.05)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=True)
+
+    args = parser.parse_args()
+    input_path = args.input
+    txt_file = os.path.join(input_path, "play.txt")
+    mp4_file = os.path.join(input_path, "video.mp4")
+    with open(txt_file, 'r') as f:
+        frame_raw = f.read()
+        frame_raw = frame_raw.replace('.', ' ')
+	#f.close()
+    frames = frame_raw.split('SPLIT')
+	#os.system(f'mplayer -vo null -vc null ${mp4_file} &')
+    init_time = time.time()
+    while time.time() <= init_time + 218:
+        os.system('clear')
+        print(frames[int((time.time()-init_time)*10)])
+        time.sleep(0.05)
 

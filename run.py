dbe18ced8cc7083698dc36d3d5cfcc46dbc97c03
Yokohide0317 <yokohide0317@gmail.com>
2022-04-30 11:11:05 +0900

    Keyboard

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import time, os
 import argparse
-
+import keyboard
 
 if __name__ == '__main__':
 
@@ -16,11 +16,15 @@ if __name__ == '__main__':
         frame_raw = frame_raw.replace('.', ' ')
 	#f.close()
     frames = frame_raw.split('SPLIT')
-    os.system(f'mplayer -vo null -vc null {mp4_file} &')
+    os.system(f'mplayer -quiet -vo null -vc null {mp4_file} &')
     init_time = time.time()
+    
     while time.time() <= init_time + 218:
         os.system('clear')
         print(frames[int((time.time()-init_time)*10)])
         time.sleep(0.05)
+        if keyboard.is_pressed("q"):
+            print("q pressed, ending loop")
+            break
     time.sleep(1)
     exit()
